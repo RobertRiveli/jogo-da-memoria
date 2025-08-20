5d912c16c1a454ab596e43e32d11c5c718da9b2b
Rivelino Sousa <rivelinosousa92@gmail.com>
2025-08-20 13:42:04 -0300

    feat: adiciona modal de dificuldades do jogo
    - adiciona modal que contém as dificuldades do jogo
    - adiciona estilos no modal
    - adiciona ação de abrir o modal ao clicar em "jogar"

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const btnJogar = document.getElementById("btn-jogo");
+const modalModos = document.getElementById("modal-modos");
+
+btnJogar.addEventListener("click", () => {
+  modalModos.showModal();
+});
